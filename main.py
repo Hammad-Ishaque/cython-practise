0997bb092aa811644415b22aefa326c50c67f955
Muhammad Hammad <m.hammad@nextgeni.net>
2021-07-27 16:08:57 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cython
+import pyximport
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    import helloworld
+    helloworld.say_hello_to('hammad')
+    import primes
+    print(primes.primes(10))
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
